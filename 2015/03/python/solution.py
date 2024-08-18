fa07be733f2154345e95bb9e6197c51a08c6d7bc
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2024-08-19 01:30:51 +0200

    Solve part 2 of 2015/03 in Python

--- 2015/03/python/solution.py
+++ 2015/03/python/solution.py
@@ -28,9 +28,30 @@ def count_houses(dirs):
     return len(houses)
 
 
+def count_robot_houses(dirs):
+    cur_pos_santa = (0, 0)
+    cur_pos_robot = (0, 0)
+    houses = {}
+
+    santa_dirs = dirs[::2]
+    robot_dirs = dirs[1::2]
+
+    for d in santa_dirs:
+        cur_pos_santa = next_position(d, *cur_pos_santa)
+        houses[cur_pos_santa] = houses.get(cur_pos_santa, 0) + 1
+
+    for d in robot_dirs:
+        cur_pos_robot = next_position(d, *cur_pos_robot)
+        houses[cur_pos_robot] = houses.get(cur_pos_robot, 0) + 1
+
+    return len(houses)
+
+
+
 def main():
     dirs = fileinput.input().readline()
     print(f'Visited houses = {count_houses(dirs)}')
+    print(f'Visited houses with robot = {count_robot_houses(dirs)}')
 
 
 if __name__ == "__main__":
