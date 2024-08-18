3db396ea92da9ad2a89a40c72d808531843207ff
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2024-08-18 15:50:03 +0200

    Solve part 2 of 2015/02 in Python

--- 2015/02/python/solution.py
+++ 2015/02/python/solution.py
@@ -10,9 +10,17 @@ def total_area(dims):
     return s
 
 
+def feet_of_ribbon(dims):
+    s = 0
+    for a, b, c in dims:
+        s += 2*(a + b) + a*b*c
+    return s
+
+
 def main():
     dims = [sorted(map(int, line.strip().split('x'))) for line in fileinput.input()]
     print(f'Total area needed = {total_area(dims)}')
+    print(f'Total ribbon length = {feet_of_ribbon(dims)}')
 
 
 if __name__ == "__main__":
