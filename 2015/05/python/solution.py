fb5a2202e17f0e448185409b1593000b43ed16db
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2024-08-21 09:51:03 +0200

    Solve part 2 of 2015/05 in Python

--- 2015/05/python/solution.py
+++ 2015/05/python/solution.py
@@ -27,13 +27,35 @@ def is_nice_string(string):
            not has_forbidden_string(string))
 
 
+def is_even_nicer_string(string):
+    def has_pair_twice(s):
+        for i in range(len(s)-2):
+            pair = s[i:i+2]
+            subs = s[:i] + '**' + s[i+2:]
+            if pair in subs:
+                return True
+        return False
+
+    def has_repeated_letter(s):
+        for i in range(len(s)-2):
+            if s[i] == s[i+2]:
+                return True
+        return False
+
+    return has_pair_twice(string) and has_repeated_letter(string)
+
+
 
 def main():
     """Main function"""
 
     strings = [s.strip() for s in fileinput.input()]
+
     total_nice_strings = len([s for s in strings if is_nice_string(s)])
     print(f'Total nice strings: {total_nice_strings}')
+
+    total_nicer_strings = len([s for s in strings if is_even_nicer_string(s)])
+    print(f'Total nicer strings: {total_nicer_strings}')
 
 
 if __name__ == "__main__":
