b57fc60164c3c7e0ae8f626fc04b596e5ca7e86e
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2024-08-23 18:55:48 +0200

    Use `itertools.permutations` to solve problem 9 of AoC 2015

--- 2015/09/python/solution.py
+++ 2015/09/python/solution.py
@@ -3,11 +3,19 @@
 Solution for problem 9 of AoC 2015.
 """
 
+__author__ = "Pedro Moreira"
+
 import fileinput
+from itertools import permutations
 
 
 def perms(l):
-    """Return all the permutations of the elements of `l`."""
+    """Return all the permutations of the elements of `l`.
+
+    .. note:: I didn't know about `itertools.permutations` when I wrote this :P
+    and I also did not know about that wizard shit:
+    https://stackoverflow.com/a/64341432/4088670
+    """
 
     if len(l) == 1:
         return [l]
@@ -48,9 +56,9 @@ def main():
             places.append(b)
         graph[(a, b)] = graph[(b, a)] = int(d)
 
-    ds = [dist(p, graph) for p in perms(places)]
-    print(min(ds))
-    print(max(ds))
+    ds = [dist(p, graph) for p in permutations(places)]
+    print('min dist =', min(ds))
+    print('max dist =', max(ds))
 
 
 if __name__ == "__main__":
