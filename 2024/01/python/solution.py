7285149548eddd66e9eeb2c33e176a56337555d1
Pedro Guilherme Siqueira Moreira <pedro.guilherme@espectro.eng.br>
2024-12-01 18:59:59 +0100

    Solve 2024/01 in Python

---
+++ 2024/01/python/solution.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import fileinput
+
+
+def main():
+    ll = []
+    rl = []
+    for line in fileinput.input():
+        x, y = line.split()
+        ll.append(int(x))
+        rl.append(int(y))
+
+    s = 0
+    for x, y in zip(sorted(ll), sorted(rl)):
+        s += abs(x - y)
+
+    print(s)
+
+    sc = 0
+    for n in ll:
+        sc += n * rl.count(n)
+
+    print(sc)
+
+
+if __name__ == "__main__":
+    main()
